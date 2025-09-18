aa4187a4629976ac4c92474a4028f6b1524f22f6
wendykrwn <wendykrwn@gmail.com>
2025-09-18 17:18:09 +0200

    gestion d'erreur traduit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import multer from "multer";
-import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import s3 from "./config/aws.js";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import cors from "cors";
 
 
@@ -17,7 +18,7 @@ const storage = multer.memoryStorage();
 const fileFilter = (req, file, cb) => {
   const allowedTypes = ["image/jpeg", "image/png", "image/gif"];
   if (!allowedTypes.includes(file.mimetype)) {
-    return cb(new Error("Seulement les fichiers JPG, PNG ou GIF sont autorisés"), false);
+    return cb(new Error("Please upload a JPG, PNG, or GIF image."), false);
   }
   cb(null, true);
 };
@@ -31,17 +32,18 @@ const upload = multer({
 
 
 app.post("/api/upload", (req, res, next) => {
+
   upload.single("image")(req, res, function (err) {
     if (err instanceof multer.MulterError) {
       if (err.code === "LIMIT_FILE_SIZE") {
-        return res.status(400).json({ error: "Le fichier dépasse 2MB" });
+        return res.status(400).json({ error: "The file is too large. Please upload an image under 2MB." });
       }
       return res.status(400).json({ error: err.message });
     } else if (err) {
       return res.status(400).json({ error: err.message });
     }
     if (!req.file) {
-      return res.status(400).json({ error: "Aucun fichier ou type invalide" });
+      return res.status(400).json({ error: "No file selected or invalid file type. Please upload a JPG, PNG, or GIF under 2MB." });
     }
     next();
   });
@@ -69,6 +71,29 @@ app.post("/api/upload", (req, res, next) => {
   }
 });
 
+app.get("/api/download/:filename",async (req,res)=>{
+  try {
+    const filename = req.params.filename;
+
+    const command = new GetObjectCommand({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: `uploads/${filename}`,
+    });
+
+    const data = await s3.send(command); 
+    if (!data.Body) throw new Error("Body S3 vide");
+
+    res.setHeader("Content-Disposition", `attachment; filename="${filename}"`);
+    res.setHeader("Content-Type", data.ContentType || "application/octet-stream");
+
+    const stream = data.Body; 
+    stream.pipe(res);
+
+  } catch (err) {
+    console.error("Erreur download:", err);
+    res.status(500).json({ error: "Unable to upload the image. Please try again." });
+  }
+})
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
